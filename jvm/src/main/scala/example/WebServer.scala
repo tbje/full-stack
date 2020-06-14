2164bb80ef22af5e6ef5e330c67f4238724d2348
Trond Bjerkestrand <trond.bjerkestrand@gmail.com>
2020-06-14 09:10:41 +0200

    Initial
    Signed-off-by: Trond Bjerkestrand <trond.bjerkestrand@gmail.com>

---
+++ jvm/src/main/scala/example/WebServer.scala
@@ -0,0 +1,23 @@
+package example
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.stream.ActorMaterializer
+import com.typesafe.config.ConfigFactory
+
+object WebServer {
+  def main(args: Array[String]) = {
+    implicit val system = ActorSystem("server-system")
+
+    val config = ConfigFactory.load()
+    val interface = config.getString("http.interface")
+    val port = config.getInt("http.port")
+
+    val service = new WebService()
+    import system.dispatcher
+    Http().bindAndHandle(service.apply(), interface, port)
+
+    println(s"Server online at http://$interface:$port")
+
+  }
+}
---
+++ project/plugins.sbt
@@ -0,0 +1,12 @@
+// fast development turnaround when using sbt ~re-start
+addSbtPlugin("io.spray" % "sbt-revolver" % "0.9.1")
+
+addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.14.7" exclude("org.apache.maven", "maven-plugin-api"))
+
+addSbtPlugin("com.typesafe.sbt" % "sbt-native-packager" % "1.7.3")
+
+addSbtPlugin("org.portable-scala" % "sbt-scalajs-crossproject"      % "1.0.0")
+
+addSbtPlugin("org.scala-js" % "sbt-scalajs" % "1.1.0")
+
+addSbtPlugin("com.vmunier" % "sbt-web-scalajs" % "1.1.0")
